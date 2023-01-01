37bef6d752184103fb0745804f263b1c5eaf3c19
devthejo <jo@surikat.pro>
2023-01-01 16:21:42 +0100

    refactor: clean

--- src/iterator/create.js
+++ src/iterator/create.js
@@ -6,8 +6,8 @@ const collectionMethodsList = require("./collection-methods-list")
 
 const {
   simpleMethods: collectionSimpleMethods,
-  // limitMethods: collectionLimitMethods,
-  // reducerMethods: collectionReducerMethods,
+  limitMethods: collectionLimitMethods,
+  reducerMethods: collectionReducerMethods,
 } = collectionMethodsList
 
 const createMiddlewareComposition = require("./create-middleware-composition")
@@ -17,96 +17,100 @@ module.exports = (params = {}) => {
   const iterator = { middlewares }
   const abortSignal = ctx.require("abortSignal")
 
-  const collectionMethods = Object.entries(async).reduce(
-    (acc, [methodName, func]) => {
-      let method = func
+  const collectionMethodNames = [
+    ...collectionSimpleMethods,
+    ...collectionLimitMethods,
+    ...collectionReducerMethods,
+  ]
 
-      const inCollectionSimpleMethods =
-        collectionSimpleMethods.includes(methodName)
+  const collectionMethods = collectionMethodNames.reduce((acc, methodName) => {
+    const func = async[methodName]
 
-      let iterateeIndex
-      if (inCollectionSimpleMethods) {
-        iterateeIndex = 0
-      } else {
-        iterateeIndex = 1
-      }
-      method = async (coll, ...args) => {
-        const collectionFunc = async (...collectionArgs) => {
-          const collectionComposition = createMiddlewareComposition(
-            "collection",
-            iterator.middlewares
-          )
-          let collection = await collectionComposition(...collectionArgs)
-          if (collection === undefined) {
-            ;[collection] = collectionArgs
-          }
-          collectionArgs[0] = collection
+    const inCollectionSimpleMethods =
+      collectionSimpleMethods.includes(methodName)
 
-          let collectionName
-          if (typeof collectionArgs[collectionArgs.length - 1] === "string") {
-            collectionName = collectionArgs.pop()
-          }
+    let iterateeIndex
+    if (inCollectionSimpleMethods) {
+      iterateeIndex = 0
+    } else {
+      iterateeIndex = 1
+    }
+    const method = async (coll, ...args) => {
+      const collectionFunc = async (...collectionArgs) => {
+        const collectionComposition = createMiddlewareComposition(
+          "collection",
+          iterator.middlewares
+        )
+        let collection = await collectionComposition(...collectionArgs)
+        if (collection === undefined) {
+          ;[collection] = collectionArgs
+        }
+        collectionArgs[0] = collection
 
-          const collectionHookParam = {
-            collection,
-            methodName,
-            collectionName,
-          }
-          for (const middleware of middlewares) {
-            if (middleware.hook) {
-              await middleware.hook(collectionHookParam, "collection")
-            }
-          }
-
-          const result = await func(...collectionArgs)
-          abortSignal.throwIfAborted()
-          return result
+        let collectionName
+        if (typeof collectionArgs[collectionArgs.length - 1] === "string") {
+          collectionName = collectionArgs.pop()
         }
 
-        return ctx.fork(async () => {
-          const iterationComposition = createMiddlewareComposition(
-            "iteration",
-            iterator.middlewares
-          )
+        const collectionHookParam = {
+          collection,
+          methodName,
+          collectionName,
+        }
+        for (const middleware of middlewares) {
+          if (middleware.hook) {
+            await middleware.hook(collectionHookParam, "collection")
+          }
+        }
 
-          const iteratorCallback = args[iterateeIndex]
-          args[iterateeIndex] = async.ensureAsync(async (...iteratorArgs) =>
-            ctx.fork(async () => {
-              if (abortSignal.aborted) {
-                return
-              }
-
-              let item = await iterationComposition(...iteratorArgs)
-              if (item === undefined) {
-                ;[item] = iteratorArgs
-              }
-              iteratorArgs[0] = item
-
-              const [, index] = iteratorArgs
-              const iterationHookParam = {
-                item,
-                index,
-                methodName,
-              }
-              for (const middleware of middlewares) {
-                if (middleware.hook) {
-                  await middleware.hook(iterationHookParam, "iteration")
-                }
-              }
-
-              const result = await iteratorCallback(...iteratorArgs)
-              return result
-            })
-          )
-
-          return collectionFunc(coll, ...args)
-        })
+        const result = await func(...collectionArgs)
+        abortSignal.throwIfAborted()
+        return result
       }
 
-      acc[methodName] = method
-      return acc
+      return ctx.fork(async () => {
+        const iterationComposition = createMiddlewareComposition(
+          "iteration",
+          iterator.middlewares
+        )
+
+        const iteratorCallback = args[iterateeIndex]
+        args[iterateeIndex] = async.ensureAsync(async (...iteratorArgs) =>
+          ctx.fork(async () => {
+            if (abortSignal.aborted) {
+              return
+            }
+
+            let item = await iterationComposition(...iteratorArgs)
+            if (item === undefined) {
+              ;[item] = iteratorArgs
+            }
+            iteratorArgs[0] = item
+
+            const [, index] = iteratorArgs
+            const iterationHookParam = {
+              item,
+              index,
+              methodName,
+            }
+            for (const middleware of middlewares) {
+              if (middleware.hook) {
+                await middleware.hook(iterationHookParam, "iteration")
+              }
+            }
+
+            const result = await iteratorCallback(...iteratorArgs)
+            return result
+          })
+        )
+
+        return collectionFunc(coll, ...args)
+      })
     }
-  )
+
+    acc[methodName] = method
+    return acc
+  }, {})
   Object.assign(iterator, collectionMethods)
   return iterator
 }
