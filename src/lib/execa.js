7766f4b029291c74f6c960d76d34cb20bebc39d4
devthejo <jo@surikat.pro>
2023-03-02 13:21:59 +0100

    fix: improve execa

--- src/lib/execa.js
+++ src/lib/execa.js
@@ -1,10 +1,12 @@
 const defaults = require("lodash.defaults")
 const pick = require("lodash.pick")
 const omit = require("lodash.omit")
-
+const { parse } = require("shell-quote")
 const { execa } = require("@esm2cjs/execa")
+const chalk = require("chalk")
 
 const ctx = require("~/ctx")
+
 const sudoFactory = require("./sudo-factory")
 
 const f10sExecaOptions = [
@@ -19,25 +21,29 @@ const f10sExecaOptions = [
 ]
 
 module.exports = async (command, args, options) => {
-  if (args && !Array.isArray(args)) {
-    options = args
-    args = null
+  if (!Array.isArray(args)) {
+    options = args || {}
+    ;[command, ...args] = parse(command)
   }
 
   let commandFunction = execa
 
-  const execaOptions = omit(options, f10sExecaOptions)
-  const extraOptions = pick(options, f10sExecaOptions)
+  const defaultExecaOptions = {}
 
-  execaOptions.all = true
+  const execaOptions = {
+    ...defaultExecaOptions,
+    ...omit(options, f10sExecaOptions),
+  }
+  const extraOptions = pick(options, f10sExecaOptions)
 
   const signal = ctx.require("abortSignal")
   const config = ctx.require("config")
 
-  const { callbacks = [] } = extraOptions
+  const callbacks = [...(extraOptions.callbacks || [])]
+
+  const { sudo } = extraOptions
 
   if (extraOptions.sudo) {
-    const { sudo } = extraOptions
     if (sudo === true) {
       commandFunction = ctx.require("sudo")
     } else {
@@ -54,8 +60,12 @@ module.exports = async (command, args, options) => {
     logCommand = config.logCommands,
   } = extraOptions
 
+  const prefix = `${logger.getPrefix()} ${chalk.grey(sudo ? "#" : "$")} `
+  const log = logger.child({})
+  log.setPrefix(prefix)
+
   if (logStdout || logStderr) {
-    const logStream = logger.createStream(logStdLevel)
+    const logStream = log.createStream({ level: logStdLevel })
 
     callbacks.unshift((child) => {
       if (logStdout) {
@@ -72,8 +82,9 @@ module.exports = async (command, args, options) => {
   defaults(execaOptions, defaultOptions)
 
   if (logCommand) {
-    logger[logStdLevel]([command, ...(args || [])].join(" "))
+    log[logStdLevel]([command, ...args].join(" "))
   }
+  log.setPrefix(`${prefix}${chalk.grey("-")} `)
 
   const child = commandFunction(command, args, execaOptions)
 
