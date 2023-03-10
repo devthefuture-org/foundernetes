d0905dd38907e02248ed96369e2610801ce32fad
devthejo <jo@surikat.pro>
2023-03-10 10:49:58 +0100

    feat: yaml detect

--- src/utils/yaml.js
+++ src/utils/yaml.js
@@ -1,10 +1,27 @@
 const yaml = require("yaml")
 const jsYaml = require("js-yaml")
 
-const load = (input, retroCompat = true) =>
+const loadObject = (input, { retroCompat = true } = {}) =>
   retroCompat
     ? yaml.parse(input.toString(), { schema: "yaml-1.1" })
     : jsYaml.load(input)
+
+const detect = (input) => {
+  if (input.trimStart().slice(0, 1) === "[") {
+    return "array"
+  }
+  const lines = input.split("\n").filter((line) => line.trim().startsWith("#"))
+  return lines.some((line) => line.startsWith("---")) ? "array" : "object"
+}
+
+const load = (input, { isArray, retroCompat = true } = {}) => {
+  if (isArray === undefined) {
+    isArray = detect(input) === "array"
+  }
+  return isArray
+    ? yaml.loadAll(input, { retroCompat })
+    : loadObject(input, { retroCompat })
+}
 
 // module.exports.dump = (input) => yaml.stringify(input)
 const dump = (input) => jsYaml.dump(input)
@@ -12,9 +29,9 @@ const dump = (input) => jsYaml.dump(input)
 const dumpAll = (manifests) =>
   manifests.map((manifest) => dump(manifest)).join("---\n")
 
-const loadAll = (input, retroCompat = true) => {
+const loadAll = (input, { retroCompat = true } = {}) => {
   if (input.trimStart().slice(0, 1) === "[") {
-    const obj = load(`arr: ${input}`, retroCompat)
+    const obj = loadObject(`arr: ${input}`, { retroCompat })
     return obj.arr
   }
 
@@ -23,7 +40,7 @@ const loadAll = (input, retroCompat = true) => {
   const append = (arr) => {
     const doc = arr.join("\n").trim()
     if (doc.length > 0) {
-      const obj = load(doc, retroCompat)
+      const obj = loadObject(doc, { retroCompat })
       if (obj !== null) {
         documents.push(obj)
       }
@@ -47,7 +64,9 @@ const loadAll = (input, retroCompat = true) => {
 module.exports = {
   parse: yaml.parse,
   load,
+  loadObject,
   loadAll,
   dump,
   dumpAll,
+  detect,
 }
