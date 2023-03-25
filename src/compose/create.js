56e2fd814e84f31ec08a2968957f047041b7c375
devthejo <jo@surikat.pro>
2023-03-25 18:24:10 +0100

    fix: improve compose tags

--- src/compose/create.js
+++ src/compose/create.js
@@ -2,14 +2,21 @@ const matchTags = require("~/std/match-tags")
 
 const ctx = require("~/ctx")
 
-module.exports =
-  async (func) =>
-  async (vars = {}, options = {}) =>
+module.exports = async (func) => {
+  const {
+    tags: createTags = [],
+    defaultTags: createDefaultTags = ["*"], // by default compose are not filtered when using tags option
+  } = func
+  return async (vars = {}, options = {}) =>
     ctx.fork(async () => {
       const { tags: playTags = [] } = options
-      const tags = [...playTags]
+      const tags = [...createTags, ...playTags]
+      if (tags.length === 0) {
+        tags.push(...createDefaultTags)
+      }
       if (!matchTags(tags, vars)) {
         return
       }
       return func(vars)
     })
+}
--- src/std/factory-of-factory.js
+++ src/std/factory-of-factory.js
@@ -1,20 +1,25 @@
+const defaults = require("lodash.defaults")
 const getCallerPath = require("~/utils/get-caller-path")
 
-module.exports = (create) => (definition) => {
-  const callerPath = getCallerPath()
-  return async (config = {}, override = {}) => {
-    if (typeof definition === "function") {
-      if (typeof config === "function") {
-        config = await config()
+module.exports =
+  (create) =>
+  (definition, factoryDefaults = {}) => {
+    const callerPath = getCallerPath()
+    return async (config = {}, override = {}) => {
+      if (typeof definition === "function") {
+        if (typeof config === "function") {
+          config = await config()
+        }
+        definition = await definition(config)
       }
-      definition = await definition(config)
-    }
-    if (typeof override === "function") {
-      override = await override(definition)
-    }
+      if (typeof override === "function") {
+        override = await override(definition)
+      }
 
-    Object.assign(definition, { path: callerPath, ...override })
+      Object.assign(definition, { path: callerPath, ...override })
 
-    return create(definition)
+      defaults(definition, factoryDefaults)
+
+      return create(definition)
+    }
   }
-}
