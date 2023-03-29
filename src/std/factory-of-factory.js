60ba3b82dd5c81dde32b3cf092a837779b33060f
devthejo <jo@surikat.pro>
2023-03-29 20:39:15 +0200

    chore: cleaning

--- src/std/factory-of-factory.js
+++ src/std/factory-of-factory.js
@@ -1,25 +1,22 @@
 const defaults = require("lodash.defaults")
-const getCallerPath = require("~/utils/get-caller-path")
 
 module.exports =
   (create) =>
-  (definition, factoryDefaults = {}) => {
-    const callerPath = getCallerPath()
-    return async (config = {}, override = {}) => {
-      if (typeof definition === "function") {
-        if (typeof config === "function") {
-          config = await config()
-        }
-        definition = await definition(config)
+  (definition, factoryDefaults = {}) =>
+  async (config = {}, override = {}) => {
+    if (typeof definition === "function") {
+      if (typeof config === "function") {
+        config = await config()
       }
-      if (typeof override === "function") {
-        override = await override(definition)
-      }
-
-      Object.assign(definition, { path: callerPath, ...override })
-
-      defaults(definition, factoryDefaults)
-
-      return create(definition)
+      definition = await definition(config)
     }
+    if (typeof override === "function") {
+      override = await override(definition)
+    }
+
+    Object.assign(definition, override)
+
+    defaults(definition, factoryDefaults)
+
+    return create(definition)
   }
