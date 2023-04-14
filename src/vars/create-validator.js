d2e25c96dc6da4c54b2b413cc279fa53f7672bbe
devthejo <jo@surikat.pro>
2023-04-14 19:56:55 +0200

    feat: add custom validator to ajv

--- src/vars/create-validator.js
+++ src/vars/create-validator.js
@@ -17,6 +17,48 @@ module.exports = async (schema, options = {}) => {
     // },
     ...options,
   })
+
+  ajv.addKeyword("validator", {
+    compile: (schm, parentSchema) =>
+      function validate(data) {
+        if (typeof schm === "function") {
+          const valid = schm(data)
+          if (!valid) {
+            validate.errors = [
+              {
+                keyword: "validate",
+                message: `: ${data} should pass custom validation`,
+                params: { keyword: "validate" },
+              },
+            ]
+          }
+          return valid
+        }
+
+        if (
+          typeof schm === "object" &&
+          Array.isArray(schm) &&
+          schm.every((f) => typeof f === "function")
+        ) {
+          const [f, errorMessage] = schm
+          const valid = f(data)
+          if (!valid) {
+            validate.errors = [
+              {
+                keyword: "validate",
+                message: `: ${errorMessage(schm, parentSchema, data)}`,
+                params: { keyword: "validate" },
+              },
+            ]
+          }
+          return valid
+        }
+
+        throw new Error("Invalid definition for custom validator")
+      },
+    errors: true,
+  })
+
   const validate = options.loadSchema
     ? await ajv.compileAsync(schema)
     : ajv.compile(schema)
