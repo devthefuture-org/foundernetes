ab6d2bdd85acc02db2145cddd8bc65eeece253d0
devthejo <jo@surikat.pro>
2023-06-10 20:28:39 +0200

    feat: storage2 and node-storage

--- plugins/linux/plays/services/lxd/index.js
+++ plugins/linux/plays/services/lxd/index.js
@@ -1,22 +1,49 @@
+const path = require("path")
+const fs = require("fs-extra")
+const yaml = require("@foundernetes/std/yaml")
+const deepmerge = require("@foundernetes/std/deepmerge")
+
 const { createComposer } = require("@foundernetes/blueprint")
+const ctx = require("@foundernetes/ctx")
 
 module.exports = async ({ children, plays }) =>
   createComposer(
     async (vars = {}) => {
-      const { storageDir = "/storage" } = vars
-      await plays.std.ensureDir(
-        {
-          dir: storageDir,
-          sudoWrite: true,
-        },
-        { tags: ["lxd"] }
+      const { storageDirs = ["/storage"] } = vars
+
+      const iterator = ctx.require("iterator")
+
+      const { instances: userInstances = {} } = vars
+      const defaultInstancesYaml = await fs.readFile(
+        path.join(__dirname, "instances.yaml"),
+        { encoding: "utf-8" }
       )
+      const defaultInstances = yaml.load(defaultInstancesYaml)
+      const instances = deepmerge({}, defaultInstances, userInstances)
+
+      await iterator.each(storageDirs, async (storageDir) => {
+        await plays.std.ensureDir(
+          {
+            dir: `${storageDir}/shared`,
+            sudoWrite: true,
+          },
+          { tags: ["lxd"] }
+        )
+        await iterator.eachOf(instances.nodes, async (_, name) => {
+          await plays.std.ensureDir(
+            {
+              dir: `${storageDir}/nodes/${name}`,
+              sudoWrite: true,
+            },
+            { tags: ["lxd"] }
+          )
+        })
+      })
 
       await children.host({}, { tags: ["lxd"] })
       const { preseed } = vars
       await children.user({}, { tags: ["lxd"] })
       await children.init({ preseed }, { tags: ["lxd"] })
-      const { instances } = vars
       await children.instances({ instances }, { tags: ["lxd"] })
     },
     { tags: ["lxd"] }
--- plugins/linux/plays/services/lxd/instances.js
+++ plugins/linux/plays/services/lxd/instances.js
@@ -1,22 +1,11 @@
-const path = require("path")
-const fs = require("fs-extra")
-
 const ctx = require("@foundernetes/ctx")
-const yaml = require("@foundernetes/std/yaml")
 const deepmerge = require("@foundernetes/std/deepmerge")
 const { createComposer } = require("@foundernetes/blueprint")
 
 module.exports = async ({ mod }) => {
   return createComposer(async (vars) => {
     const iterator = ctx.require("iterator")
-    const { instances: userInstances = {} } = vars
-    const defaultInstancesYaml = await fs.readFile(
-      path.join(__dirname, "instances.yaml"),
-      { encoding: "utf-8" }
-    )
-    const defaultInstances = yaml.load(defaultInstancesYaml)
-
-    const instances = deepmerge({}, defaultInstances, userInstances)
+    const { instances } = vars
 
     let index = 0
     await iterator.eachOfSeries(instances.nodes, async (instance, name) => {
