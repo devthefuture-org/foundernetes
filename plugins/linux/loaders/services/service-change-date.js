9951836adde080175f364d023d9470a03b0e7e95
devthejo <devthejo@protonmail.com>
2023-10-28 15:15:25 +0200

    fix: service reload date from journalctl

--- plugins/linux/loaders/services/service-change-date.js
+++ plugins/linux/loaders/services/service-change-date.js
@@ -1,15 +1,38 @@
 const { createLoader } = require("@foundernetes/blueprint")
+const dayjs = require("dayjs")
+const { $ } = require("@foundernetes/blueprint")
 
 module.exports = ({ loaders }) =>
   createLoader({
     load: async (vars) => {
       const { name } = vars
+
       const stateTimestamp = await loaders.services.serviceInfos({
         name,
         field: "StateChangeTimestamp",
       })
       const [, day, time, tz] = stateTimestamp.split(" ")
-      const date = new Date(`${day} ${time} (${tz})`)
+      const dateFromSystemCtl = new Date(`${day} ${time} (${tz})`)
+
+      const { stdout: journalstdout } = await $(
+        `journalctl -u ${name} --no-pager -o short-iso -xe`,
+        {
+          sudo: true,
+          logStdout: false,
+        }
+      )
+      const lastRestartLine = journalstdout
+        .split("\n")
+        .reverse()
+        .find((line) => line.includes("Starting") || line.includes("Reloaded"))
+      const [lastRestartIsoTime] = lastRestartLine.split(" ")
+      const dateFromJournalCtl = dayjs(lastRestartIsoTime).toDate()
+
+      const date =
+        dateFromSystemCtl > dateFromJournalCtl
+          ? dateFromSystemCtl
+          : dateFromJournalCtl
+
       return date
     },
   })
